755809acaf7d518b90e80861bfe0364503d33f3e
kangtae210 <kangtae210@naver.com>
2021-09-15 20:30:49 +0900

    study

--- Two_Sum.py
+++ Two_Sum.py
@@ -2,6 +2,7 @@
 # 정수 리스트 nums와 정수 target이 주어져있다.
 # 리스트 nums의 두 원소를 합하면 target 값이 되는
 # 두 지수값을 리스트로 하는 리스트 rtype을 반환하는 함수를 작성하라.
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
